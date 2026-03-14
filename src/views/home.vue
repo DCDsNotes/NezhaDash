539b87144e4000ee4e82bd67f3a4641c61d33939
DCDs Notes <dcdsnotes@gmail.com>
2026-03-14 11:44:19 +0800

    Upload Files

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,8 +1,8 @@
 <template>
   <div
     class="index-container"
-      :class="indexContainerClass"
-    >
+    :class="indexContainerClass"
+  >
     <div
       ref="scrollContainerRef"
       class="scroll-container"
